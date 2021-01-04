ec8ccb8a2a18ef4c57e153184dbaf82cbaf6a06a
Scott Doerrfeld <scott@wowzers.com>
2021-01-03 16:35:42 -0800

    Just defer to app on Windows for now

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -37,7 +37,7 @@ module OmniAuth
           if stored_state && (!request.params["state"] || request.params["state"] != stored_state)
             fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
           # On desktop platforms we defer to the app
-          elsif !request.params["skip_app_defer"] && (request.user_agent =~ /Windows/ || request.user_agent =~ /Mac/)
+          elsif !request.params["skip_app_defer"] && (request.user_agent =~ /Windows/)
             call_app!
           else
             super
